5aa693c20a8970a7fa9f3a3e4443a0b6d889f334
Syarol <1oleq25@gmail.com>
2018-04-27 22:51:23 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 50.7472, lng: 25.3254},
+          zoom: 12
+        });
+      }
+    
+   
