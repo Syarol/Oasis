b1df0150da25e75f490c3fcee331eaab8a590296
Syarol <1oleq25@gmail.com>
2018-05-01 19:27:20 +0300

    Add styles for search page and improve for others

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
 
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 50.7472, lng: 25.3254},
-          zoom: 12
-        });
-      }
-    
-   
+for (item of document.querySelectorAll('.arrival-item-inf h3')){
+    if (item.textContent.length > 12) item.style.fontSize  = '1em'
+}
+
+
+var closeModal = document.getElementById('close-modal');
+
+closeModal.addEventListener('click', function(){
+    document.getElementById('bestseller_modal_wrapper').style.display = 'none';
+})
+
+document.getElementById('open_bestseller_modal').addEventListener('click', function(){
+    document.getElementById('bestseller_modal_wrapper').style.display = 'flex';
+})
+
+window.onclick = function(e) {
+  if (e.target == document.getElementById('bestseller_modal_wrapper')) {
+    document.getElementById('bestseller_modal_wrapper').style.display = 'none';
+  }
+}; 
